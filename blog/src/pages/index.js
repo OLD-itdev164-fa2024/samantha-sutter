f8e1238f39b60d4fb84200ad1a28800730b632b7
SamanthaSutter <111908649+SamanthaSutter@users.noreply.github.com>
2023-09-26 21:53:38 -0500

    Activity Three

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -4,6 +4,7 @@ import {graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -13,6 +14,13 @@ const IndexPage = ({ data }) => (
         data.allContentfulBlogPost.edges.map(edge => (
           <li key={edge.node.id}>
             <Link to={edge.node.slug}>{edge.node.title}</Link>
+            <div>
+              <GatsbyImage image={edge.node.heroImage.gatsbyImageData}>
+              </GatsbyImage>
+            </div>
+            <div>
+              {edge.node.body.childrenMarkdownRemark}
+            </div>
           </li>
         ))
       }
@@ -20,23 +28,26 @@ const IndexPage = ({ data }) => (
   </Layout>
 )
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title="Home" />
-
-export default IndexPage
-
 export const query = graphql`
-  query {
+{
     allContentfulBlogPost {
       edges {
         node {
-          slug
           id
           title
+          slug
+          body {
+            childMarkdownRemark {
+              excerpt
+            }
+          }
+          heroImage {
+            gatsbyImageData(
+              layout: CONSTRAINED
+              placeholder: BLURRED
+              width:300
+            )
+          }
         }
       }
     }
