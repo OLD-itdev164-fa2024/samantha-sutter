e6abde1f8f3fe3763eaf791baab70dbf4af137cf
SamanthaSutter <111908649+SamanthaSutter@users.noreply.github.com>
2023-10-16 21:37:36 -0500

    Completed Activity 5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -6,7 +6,7 @@ require('dotenv').config({
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog`,
+    title: `Sami's Site`,
     description: `ITDEV-164 Gatsby Blog.`,
     author: `Samantha Sutter`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
